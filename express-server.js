1ea3b58d2e97025d7b4451491866160c64eaed78
Juliana Porto <jvporto@wpi.edu>
2023-11-20 23:21:08 -0500

    updated server routes and buttons

--- express-server.js
+++ express-server.js
@@ -1,27 +1,59 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const db = require('./queries')
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const db = require('./queries');
 const path = require('path');
+const { request } = require('http');
 const port = 3000
+const router = express.Router();
 
 // Middleware to add json and bodyParser
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
-)
+);
+app.use(express.static("views"));
 
-// Direct to login page on window load
+
+// Page Directory
+
+// Direct to login page
 app.get('/', (request, response) => {
   response.sendFile(path.join(__dirname, '/views/index.html'));
-})
+});
+
+// Direct to home page
+app.get('/goHome', (request, response) => {
+  response.sendFile(path.join(__dirname, '/views/home.html'));
+});
+
+// Direct to create account page
+app.get('/goCreateAccount', (request, response) => {
+  response.sendFile(path.join(__dirname, '/views/createAccount.html'));
+});
+
+// Direct to user page
+app.get('/goUserPage', (request, response) => {
+  response.sendFile(path.join(__dirname, '/views/user.html'));
+});
+
+// Direct to change pass page
+app.get('/goChangePass', (request, response) => {
+  response.sendFile(path.join(__dirname, '/views/changepass.html'));
+});
+
+
+// Table requests
 
 // Gets all plants
-app.get('/plants', db.getPlants)
+app.get('/plants', db.getPlants);
 // Gets plant by plantID - Does not work
-app.get('/plants/:plantID', db.getPlantById)
+app.get('/plants/plantID', db.getPlantById);
+
+// testing queries
+app.get('/test', db.showUserFavoritePosts);
 
 // app.post('/users', db.createUser)
 // app.put('/users/:id', db.updateUser)
@@ -30,4 +62,4 @@ app.get('/plants/:plantID', db.getPlantById)
 // Start localhost
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
-})
+});
